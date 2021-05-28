6df40eb1c232d19af6a8b447af550c2c3d168b20
Daniel Huber <hubdan@protonmail.ch>
2021-05-28 08:37:31 +0200

    FormatHex macro prints based on nibbles

--- src/kernelmain.asm
+++ src/kernelmain.asm
@@ -8,11 +8,40 @@
 ; first stage bootloader loads the sectors into memory
 %include "src/bootsector.asm"
 
-; sets up interrupts, allows for exception handling
+; sets up interrupts and exception handling
 %include "src/interrupts.asm"
 
 
-; endless loop
+
+jmp skip_includes
+; here are all the include files that provide functionality to other programs.
+; They do not need to run and the functions in these files will only be called
+; explicitly.
+
+; allows kernel code to call string formatting functions
+%include "src/formats.asm"
+
+
+
+skip_includes:
+
+
+xor eax, eax
+mov es, ax
+mov ds, ax
+
+
+
+
+
+
 jmp $
+
+;String Buffers
+greets: db "Welcome to FlamingOS!    "
+strbuf: times 65 db " "
+db 0
+
+
 ; padd out so it aligns with a multiple of 512
 times MAX_SECTORS*512-($-$$) db 0
