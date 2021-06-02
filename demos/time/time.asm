bd3a28bb4371e3bbc5e3f44583346dc9f2cb30ab
Moray <Moray___01@outlook.com>
2021-06-02 14:18:32 +0200

    Created a demo programm that shows the current time.

---
+++ demos/time/time.asm
@@ -0,0 +1,52 @@
+
+
+mov edi, 0xb8000
+add edi, 1280
+
+
+mov ah, 0x0
+clc 
+int 0x1a
+
+
+mov ax, cx
+rol eax, 16
+mov ax, dx
+mov edx, 0
+mov ecx, 65520
+div ecx
+mov cl, 10
+div cl
+mov dl, ah
+call print
+mov al, dl
+add al, 2
+call print
+mov al, 0xa
+call print
+mov ax, dx
+mov cx, 1092
+mov dx, 0
+div cx
+mov cl, 10
+div cl
+mov dl, ah
+call print
+mov al, dl
+call print
+jmp end
+
+
+
+
+print:
+mov ah, 0000_1111b
+add al, 0x30
+mov [edi],ax
+add edi, 2
+ret
+
+
+
+end:
+END_PADDING
