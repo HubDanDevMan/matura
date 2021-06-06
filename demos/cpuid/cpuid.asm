8384ea787a0d113e2c1d2f9f0cdb443d5cd8c197
Nick <nigi03@gmx.ch>
2021-06-06 03:23:35 +0200

    Cpuid brandbuffer fix

--- demos/cpuid/cpuid.asm
+++ demos/cpuid/cpuid.asm
@@ -1,100 +1,132 @@
-%include "firststage.asm" 
-
 jmp ___main
 
-%include "printHex.asm"				;include printBuff and formatHex
+%include "cpuid/printHex.asm"				;include printBuff and formatHex
 %define LINE_WIDTH 80		
 %define FREE_LINE (LINE_WIDTH-5)
 	
 ___main:
-xor eax, eax						;set eax to 0
+xor eax, eax						;cpuid leaf 0
 mov ds, ax							
 mov es, ax
 
-cpuid								;if eax=0 
-mov edi, buffm
-mov [edi], ebx
-mov [edi+4], edx
-mov [edi+8], ecx
+cpuid								;if eax=0: returns the CPU's manufacturer ID string which is always 12 characters long
+mov edi, buffmanu					;move a buffer into edi before characters are moved into the buffer
+mov [edi], ebx						;first 4 characters
+mov [edi+4], edx					;next 4 characters
+mov [edi+8], ecx					;last 4 characters
 
-mov eax, 0x80000001
-cpuid
 
-bt edx, 29
+
+mov eax, 0x80000001					
+cpuid								;if eax=0x80000001: returns feature flags about the CPU in EDX and ECX
+bt edx, 29							;the 29th bit of edx is 1 if the CPU supports 64-bit instructions and registers
 jc longmodes
 
-longmodens:
-mov cx, 22 
+longmodens:							;overwrites Longmodeyes with Longmode no 
+mov cx, 22 							 
 mov edi, Longmodeyes
 mov esi, Longmodeno
-rep movsb
+rep movsb							;22 characters are moved from esi to edi
 
-longmodes:
-mov edi, bufflm
-mov esi, Longmodeyes
+longmodes:							
+mov edi, bufflm						;move a buffer into edi and 
+mov esi, Longmodeyes				;Longmodeyes into esi so that printBuff can print the string
 
 modelid:
-mov eax, 1 
-cpuid
+mov eax, 1 							;cpuid leaf 1
+cpuid								;if eax=1: returns information about the family and model of the CPU in eax
 
-push eax
-and eax, 0x00000f00
-shr eax, 8
-mov edi, fambuff
-call formatHex
-shl eax, 8
-cmp ah, 0x0f
-je ExtModel
-cmp ah, 0x06
-je ExtModel
+;												EAX
+; _______________________________________________________________________________________________
+;|31|30|29|28|27|26|25|24|23|22|21|20|19|18|17|16|15|41|13|21|11|10|09|08|07|06|05|04|03|02|01|00|
+;|-----------|-----------------------|-----------|-----|-----|-----------|-----------|-----------|
+;| reserved  |  extended family ID   | extended  |res- |CPU  | family ID |   model   |stepping ID|
+;| 			 |						 | model ID  |erved|type |     		 |			 |			 |
+;|___________|_______________________|___________|_____|_____|___________|___________|___________|
+
+mov esi, eax						
+and eax, 0x00000f00					;clear eax except for family ID
+cmp ah, 0x0f						;if family ID = 15 jump to ExtFam
+je ExtFam
+shr ax, 8 							;if family ID in not = 15 the family ID is the value family ID field
+mov edi, fambuff                    ;move buffer into edi for formatHex
+call formatHex						;formatHex converts content of eax into ascii and moves it into the buffer in edi; read printHex.asm for more information
+cmp al, 0x06						
+je ExtModel							;if family ID = 6 jump to ExtModel
+
 
 model:
-pop eax
-push eax
-and eax, 0x000000f0
-mov edi, modbuff
+mov eax, esi
+and eax, 0x000000f0 				;clear eax except for model ID
+mov edi, modbuff					
 call formatHex
-jmp halt
+jmp halt							
 
-ExtModel:
-pop eax
-push eax
-shr al, 4
-shl eax, 4
-and eax, 0xf00000f0
+ExtFam:								;if family ID = 15 the actual family ID is derived from extended family ID + family ID 
+mov eax, esi		
+and eax, 0x0ff00f00					;clear eax except for extended family ID and family ID
+shr eax, 4
+shl ax, 8
+shr eax, 12
+mov edi, fambuff
+call formatHex
+
+ExtModel:							;if family ID = 6 or 15 the actual model ID is derived from extended model ID + model 
+mov eax, esi				
+and eax, 0x000f00f0					;clear eax except for extended model ID and model
+shl ax, 8
+shr eax, 12
 mov edi, modbuff
 call formatHex
-jmp halt
+
+brand:
+mov edi, brandbuff					
+mov eax, 0x80000002
+cpuid
+call brandstr
+mov eax, 0x80000003
+cpuid 
+call brandstr
+mov eax, 0x80000004
+cpuid
+call brandstr
 
 halt:
-mov esi, strbuff
-call printBuff
-pop eax
-jmp $
+mov esi, strbuff					;move buffers into esi before printBuff
+call printBuff						;prints the buffers; read printHex.asm for more information
+jmp $								;halt
+
+
+brandstr:
+mov [edi], eax
+mov [edi+4], ebx
+mov [edi+8], ecx
+mov [edi+12], edx
+add edi, 16
+ret
+
 
 strbuff:
 db "CPU manufacturer: "
-strmlength equ $-strbuff
-buffm: times LINE_WIDTH-strmlength db " "
-
+strmanulength equ $-strbuff
+buffmanu: times LINE_WIDTH-strmanulength db " "
 db "Family: "
 fambuff: times FREE_LINE-3 db " "
 db "Model: "
 modbuff: times FREE_LINE-2 db " "
-;db "....: "
-;..buff: times FREE_LINE db " "
-;db "....: "
-;..buff: times FREE_LINE db " "
 
 Longmodeyes:
 db "Longmode supported"
 strlmlength equ $-Longmodeyes
 bufflm: times LINE_WIDTH-strlmlength db " "
-db 0
+
+Brandbuff:
+db "Brand: "
+strbrandlength equ $-Brandbuff
+brandbuff: times LINE_WIDTH-strbrandlength db 0
 
 Longmodeno:
 db "Longmode not supported"
 db 0
 
-
-times 510*MAX_SECTORS-($-$$) db 0
+END_PADDING
