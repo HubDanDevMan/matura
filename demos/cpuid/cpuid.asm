e11a3d64c6d201d5ecc2f5d41b37c8bd86cfb782
Brrrutusss <64127386+Brrrutusss@users.noreply.github.com>
2021-06-06 03:51:48 +0200

    fixed comment

--- demos/cpuid/cpuid.asm
+++ demos/cpuid/cpuid.asm
@@ -41,7 +41,7 @@ cpuid								;if eax=1: returns information about the family and model of the CP
 ;|31|30|29|28|27|26|25|24|23|22|21|20|19|18|17|16|15|41|13|21|11|10|09|08|07|06|05|04|03|02|01|00|
 ;|-----------|-----------------------|-----------|-----|-----|-----------|-----------|-----------|
 ;| reserved  |  extended family ID   | extended  |res- |CPU  | family ID |   model   |stepping ID|
-;| 			 |						 | model ID  |erved|type |     		 |			 |			 |
+;|	     |			     | model ID  |erved|type | 		 |	     |		 |
 ;|___________|_______________________|___________|_____|_____|___________|___________|___________|
 
 mov esi, eax						
