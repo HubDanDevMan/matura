49d2945075c432a9bcc20822e9647330d957471c
Brrrutusss <nigi03@gmx.ch>
2021-06-07 12:14:41 +0200

    adjusted wordlist

--- demos/hangman/wordlist.asm
+++ demos/hangman/wordlist.asm
@@ -1,124 +1,125 @@
-db "basketball", 0
-db "football", 0
-db "volleyball", 0
-db "tennis", 0
-db "baseball", 0
-db "table", 0
-db "chair", 0
-db "television", 0
-db "couch", 0
-db "window", 0
-db "closet", 0
-db "capybara", 0
-db "otter", 0
-db "snail", 0
-db "giraffe", 0
-db "rhinoceros", 0
-db "monkey", 0
-db "gorilla", 0
-db "penguin", 0
-db "flamingo", 0
-db "cockroach", 0
-db "chameleon", 0
-db "dolphin", 0
-db "squirrel", 0
-db "hedgehog", 0
-db "hippopotamus", 0
-db "elephant", 0
-db "kangaroo", 0
-db "octopus", 0
-db "donkey", 0
-db "pangolin", 0
-db "platypus", 0
-db "scorpion", 0
-db "salamander", 0
-db "peacock", 0
-db "wombat", 0
-db "buffalo", 0
-db "python", 0
-db "windows", 0
-db "linux", 0
-db "android", 0
-db "cybercrime", 0
-db "processor", 0
-db "assembly", 0
-db "cheese", 0
-db "pizza", 0
-db "water", 0
-db "apple", 0
-db "cucumber", 0
-db "eggplant", 0
-db "rhubarb", 0
-db "salad", 0
-db "rice", 0
-db "mushroom", 0
-db "lemon", 0
-db "coconut", 0
-db "avacado", 0
-db "banana", 0
-db "cherry", 0
-db "melon", 0
-db "potato", 0
-db "pumpkin", 0
-db "onion", 0
-db "carrot", 0
-db "bread", 0
-db "hamburger", 0
-db "lobster", 0
-db "salmon", 0
-db "chocolate", 0
-db "noodles", 0
-db "sandwich", 0
-db "bacon", 0
-db "beer", 0
-db "turkey", 0
-db "wine", 0
-db "cookie", 0
-db "donut", 0
-db "fondu", 0
-db "lasagna", 0
-db "pepperoni", 0
-db "england", 0
-db "india", 0
-db "germany", 0
-db "switzerland", 0
-db "italy", 0
-db "france", 0
-db "sweden", 0
-db "europe", 0
-db "austria", 0
-db "finland", 0
-db "croatia", 0
-db "greece", 0
-db "russia", 0
-db "portugal", 0
-db "norway", 0
-db "poland", 0
-db "africa", 0
-db "asia", 0
-db "japan", 0
-db "china", 0
-db "korea", 0
-db "trap", 0
-db "abyss", 0
-db "piano", 0
-db "violin", 0
-db "flute", 0
-db "guitar", 0
-db "bongo", 0
-db "lute", 0
-db "trumpet", 0
-db "ukulele", 0
-db "bagpipe", 0
-db "galaxy", 0
-db "star", 0
-db "moon", 0
-db "mars", 0
-db "jupiter", 0
-db "mercury", 0
-db "venus", 0
-db "earth", 0
-db "saturn", 0
-db "uranus", 0
-db "neptune", 0
-db "hangman", 0
+str1	db "basketball", 0
+str2	db "football", 0
+str3	db "volleyball", 0
+str4	db "tennis", 0
+str5	db "baseball", 0
+str6	db "table", 0
+str7	db "chair", 0
+str8	db "television", 0
+str9	db "couch", 0
+str10	db "window", 0
+str11	db "closet", 0
+str12	db "capybara", 0
+str13	db "otter", 0
+str14	db "snail", 0
+str15	db "giraffe", 0
+str16	db "rhinoceros", 0
+str17	db "monkey", 0
+str18	db "gorilla", 0
+str19	db "penguin", 0
+str20	db "flamingo", 0
+str21	db "cockroach", 0
+str22	db "chameleon", 0
+str23	db "dolphin", 0
+str24	db "squirrel", 0
+str25	db "hedgehog", 0
+str26	db "hippopotamus", 0
+str27	db "elephant", 0
+str28	db "kangaroo", 0
+str29	db "octopus", 0
+str30	db "donkey", 0
+str31	db "pangolin", 0
+str32	db "platypus", 0
+str33	db "scorpion", 0
+str34	db "salamander", 0
+str35	db "peacock", 0
+str36	db "wombat", 0
+str37	db "buffalo", 0
+str38	db "python", 0
+str39	db "windows", 0
+str40	db "linux", 0
+str41	db "android", 0
+str42	db "cybercrime", 0
+str43	db "processor", 0
+str44	db "assembly", 0
+str45	db "cheese", 0
+str46	db "pizza", 0
+str47	db "water", 0
+str48	db "apple", 0
+str49	db "cucumber", 0
+str50	db "eggplant", 0
+str51	db "rhubarb", 0
+str52	db "salad", 0
+str53	db "rice", 0
+str54	db "mushroom", 0
+str55	db "lemon", 0
+str56	db "coconut", 0
+str57	db "avacado", 0
+str58	db "banana", 0
+str59	db "cherry", 0
+str60	db "melon", 0
+str61	db "potato", 0
+str62	db "pumpkin", 0
+str63	db "onion", 0
+str64	db "carrot", 0
+str65	db "bread", 0
+str66	db "hamburger", 0
+str67	db "lobster", 0
+str68	db "salmon", 0
+str69	db "chocolate", 0
+str70	db "noodles", 0
+str71	db "sandwich", 0
+str72	db "bacon", 0
+str73	db "beer", 0
+str74	db "turkey", 0
+str75	db "wine", 0
+str76	db "cookie", 0
+str77	db "donut", 0
+str78	db "fondu", 0
+str79	db "lasagna", 0
+str80	db "pepperoni", 0
+str81	db "england", 0
+str82	db "india", 0
+str83	db "germany", 0
+str84	db "switzerland", 0
+str85	db "italy", 0
+str86	db "france", 0
+str87	db "sweden", 0
+str88	db "europe", 0
+str89	db "austria", 0
+str90	db "finland", 0
+str91	db "croatia", 0
+str92	db "greece", 0
+str93	db "russia", 0
+str94	db "portugal", 0
+str95	db "norway", 0
+str96	db "poland", 0
+str97	db "africa", 0
+str98	db "asia", 0
+str99	db "japan", 0
+str100	db "china", 0
+str101	db "korea", 0
+str102	db "trap", 0 
+str103	db "abyss", 0
+str104	db "piano", 0
+str105	db "violin", 0
+str106	db "flute", 0
+str107	db "guitar", 0
+str108	db "bongo", 0
+str109	db "lute", 0
+str110	db "trumpet", 0
+str111	db "ukulele", 0
+str112	db "bagpipe", 0
+str113	db "galaxy", 0
+str114	db "star", 0
+str115	db "moon", 0
+str116	db "mars", 0
+str117	db "jupiter", 0
+str118	db "mercury", 0
+str119	db "venus", 0
+str120	db "earth", 0
+str121	db "saturn", 0
+str122	db "uranus", 0
+str123	db "neptune", 0
+str124	db "hangman", 0
+str125	db "vinegar", 0
