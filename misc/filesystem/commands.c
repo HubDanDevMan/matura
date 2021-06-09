2de18a0a83e3ec04560487b6fe0676dcb59cd673
perro <hubdan@protonmail.ch>
2021-06-09 23:40:41 +0200

    worked on disk allocation mechanism

--- misc/filesystem/commands.c
+++ misc/filesystem/commands.c
@@ -9,12 +9,47 @@
 // look for a byte that is less than 0xFF. Only then it has to identify
 // the correct bit
 
-// GEELEDDDDD
-//
 
 
-int findEmptySector()
-{ // this function scans through the bitmap for unoccupied sectors
-	while (sectBitmap)
 
+int findEmptySectors(int sectorsNeeded)
+{ // this function scans through the bitmap for sectorsNeeded consecutive unoccupied sectors
+	int i = 0, j = 0;
+	while (sectBitmap[i] == 0xff){
+		i++;
+		if (i == TOTAL_SECTORS){ return -2;}	// Disk is completely full 
+		// calculate which bits are free
+	}
+
+	// sectBitmap contains free space
+	// sectorsNeeded
+	// parse byte for free bits
+	unsigned char res = parseByte(sectBitmap[i]);
+	
+}
+
+
+
+
+unsigned char parseByte(char byte){
+	/* 
+	 * identify 1100011i
+	 */
+
+	char highestFree = 0, bit;
+	for (int i = 0; i<8; i++){
+		//bittest
+		val = byte >> i; // parse until
+		bit = val & 1;
+		if (bit == 0)
+			highestFree++;
+			flag = 1;
+			// check next bit is used, if so sector is full
+			if ((byte >> i+1) & 1 == 1){
+				return highestFree; 	// NOTE THIS ONLY RETURNS
+							// THE NUMBER; NOT THE BIT OFFSET
+							// Register should hold the correct bit
+							// offset in ASM implementation
+			}
+	}
 }
