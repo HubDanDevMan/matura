153f8d0e87f502e23f59932e8fe056d6d3119b5f
perro <hubdan@protonmail.ch>
2021-06-13 23:23:36 +0200

    arbi

--- test/demo.asm
+++ test/demo.asm
@@ -1,21 +1,13 @@
 %include "firststage.asm"
 
-mov bx, 0xcfff
+rdseed ax
+mov ah, 0x0e
+int 0x10
 
-redo:
-	in al, 0x61
-	or al, 0x02
-	out 0x61, al
-	; add delay
-	mov cx, bx
-	repeat:
-		dec cx
-		jnz repeat
-	in al, 0x61
-	and al, 0b1111_1100
-	out 0x61, al
-	jmp redo
-	
+
+
+
+
 
 jmp $
 times 512*MAX_SECTORS-($-$$) db 0
