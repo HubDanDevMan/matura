7959144347428062654bdf1478350011bcb6c439
Brrrutusss <nigi03@gmx.ch>
2021-11-18 17:59:22 +0100

    added commentary

--- demos/keyboard/keyboard.asm
+++ demos/keyboard/keyboard.asm
@@ -1,7 +1,5 @@
 jmp _start
 
-;%include "cpuid/printHex.asm"
-
 TIME_OUT dw 65535 
 DELAY dw 65535
 ACK_RETRY db 4
@@ -12,12 +10,11 @@ SECOND_PORT db 0xd4
 ACK_FLAG: resb 1
 COMMAND_BYTE: resb 1
 DATA_BYTE: resb 1
-COMMAND_ARRAY times 8 dw 0
-KEY_LUT_1:					;Key lookuptable scan code set 1
-	db " ", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", " ", " " 
-	db "q", "w", "e", "r", "t", "z", "u", "i", "o", "p", " ", " ", " ", " ", "a", "s"
-	db "d", "f", "g", "h", "j", "k", "l", " ", " ", " ", " ", " ", "y", "x", "c", "v"
-	db "b", "n", "m", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "
+KEY_LUT_1_CH:					;Key lookuptable scan code set 1
+	db " ", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "'", "^", " ", " " 
+	db "q", "w", "e", "r", "t", "z", "u", "i", "o", "p", "ü", " ", " ", "a", "s", "d"
+	db "f", "g", "h", "j", "k", "l", "ö", "ä", "$", "y", "x", "c", "v", "b", "n", "m" 
+	db ",", ".", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "
 	db " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "
 	db " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "
 	db " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "
@@ -45,28 +42,16 @@ mov al, "E"
 int 0x10
 jmp $
 
-o:
-mov ah, 0x0e
-mov al, "O"
-int 0x10
-jmp $
-
-e:
-mov ah, 0x0e 
-mov al, "A"
-int 0x10
-jmp $
-
-initialise:
+initialise:			;set scan code set. decides what the scan code for the different keys are.
 	mov dx, 0xf0
 	mov [COMMAND_BYTE], dx
 	mov dx, 2
 	mov [DATA_BYTE], dx
 	call sendCommand
 
-	mov dx, 0xf4
+	mov dx, 0xf4		;enable scanning; sends scan code of a key to the I/O Port 0x60 (Data port)
 	mov [COMMAND_BYTE], dx
-	xor dx, dx
+	xor dx, dx		;no data byte
 	mov [DATA_BYTE],dx
 	call sendCommand
 	
@@ -74,18 +59,18 @@ initialise:
 
 
 getKey:
-	.scanCodeLoop:
+	.scanCodeLoop:		;check if a key was pressed
 	in al, 0x64
 	bt ax, 0
 	jnc .scanCodeLoop
 	xor eax, eax
 
-	in al, 0x60
-	cmp al, 0x80
-	ja .scanCodeLoop
-	mov ebx, KEY_LUT_1
-	add ebx, eax
-	mov al, byte [ebx]
+	in al, 0x60		;get scan code from data port
+	cmp al, 0x80		
+	ja .scanCodeLoop	;skip scan code when it's just a key that was released
+	mov ebx, KEY_LUT_1_CH	;move memory address of the lookup table to ebx
+	add ebx, eax		;the value of the scan code in eax equals the offset from the base memory address of the lookup table
+	mov al, byte [ebx]	;move the value in the lookup table at the offset of the scan code value into al
 	mov ah, 0x0e
 	int 0x10
 	;
@@ -154,11 +139,11 @@ sendCommand:				;move command byte into dl and if needed data byte into dh befor
 	ret
 
 
-checkResponse:				;checks wether the device sent an acknowledge or a resend
+checkResponse:				;checks wether the device sent an acknowledge or a resend (data port did or didn't receive the command)
 	push ax
 	
 	.checkAck:
-	call delay
+	call delay			;give the device time to send a response
 	dec cx
 	cmp cx, 0
 	je error
@@ -167,11 +152,7 @@ checkResponse:				;checks wether the device sent an acknowledge or a resend
 	je .retry
 	cmp al, 0xfa			;response byte for acknowledged
 	je .ack
-	cmp al, 0
-	je error
-	cmp al, 0xff
-	je error
-	jmp o
+	jmp error			;error
 
 	.ack:
 	mov dx, 1
