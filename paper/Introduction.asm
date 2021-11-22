e012b854aee5aa971876630de87b76d9e4d83c28
Brrrutusss <nigi03@gmx.ch>
2021-11-22 13:59:17 +0100

    wrote the introduction

--- paper/Introduction.asm
+++ paper/Introduction.asm
@@ -1,2 +1,6 @@
-Operating systems are essential for every working computer or mobile device. But what are operating systems exactly? In our group of three people we tried to answer that question as
+\section{Introduction}
+
+Operating systems are essential for every working computer or mobile device. In a group of three people we tried to answer the question what operating 
+systems are exactly with a booklet containing several chapters describing the most important parts of an operating system. Additionally we wrote our own 
+operating system with a few basic programs. 
 
