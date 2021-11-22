bec98f944ed2d57add14eff84e7f39b68a8aefcc
Brrrutusss <nigi03@gmx.ch>
2021-11-22 11:57:31 +0100

    backspace now works

--- demos/text_editor/text_editor.asm
+++ demos/text_editor/text_editor.asm
@@ -7,7 +7,7 @@ jmp main
 main:
 call clear_screen
 
-mov edi, 0xb8000				;move video memory to edi
+;mov edi, 0xb8000				;move video memory to edi
 mov esi, buffer					;move the memory address of the beginning of the buffer into esi
 
 key_loop:
@@ -34,11 +34,17 @@ key_loop:
 	mov byte [esi], al			;move input at cursor location into the buffer
 	call printBuffer			;prints buffer
 	inc esi
-	add edi, 2
+	;add edi, 2
 	jmp key_loop				;repeat
 
 	
 	backspace:
+	cmp esi, buffer
+	je key_loop
+	;sub edi, 2				;go back one space
+	dec esi
+	call shiftBufferLeft			;move all characters right of the deleted characters one to the left
+	call printBuffer
 	jmp key_loop
 
 	tab:
@@ -53,14 +59,14 @@ key_loop:
 	left:
 	cmp esi, buffer			;checks if the cursor is already at the beginning of the text
 	je key_loop
-	sub edi, 2
+	;sub edi, 2
 	dec esi
 	jmp key_loop
 	
 	right:
 	cmp byte [esi], 0		;checks if the cursor is already at the end of the text
 	je key_loop			;prevents the cursor from moving to far to the right
-	add edi, 2
+	;add edi, 2
 	inc esi
 	jmp key_loop
 
@@ -105,15 +111,21 @@ printBuffer:				;prints entire buffer again
 	
 	mov esi, buffer
 	mov edi, 0xb8000
-	
+
 	.printLoop:
 	mov al, byte [esi]		;print character in buffer to screen
 	mov byte [edi], al
 	inc esi
-	add edi, 2
+	;add edi, 2
 	cmp byte [esi], 0
 	jne .printLoop			;next character
-	
+
+nop
+nop
+nop
+nop
+	.continue:
+
 	pop esi
 	pop edi
 
@@ -128,8 +140,7 @@ shiftBufferRight:
 	.checkBufferLoop:
 	inc cx				;counts the number of characters in the buffer on the right side of the cursor
 	inc esi
-	mov dl, byte [esi]		;check for the end of the buffer
-	cmp dl, 0
+	cmp byte [esi], 0		;check for the end of the buffer
 	jne .checkBufferLoop
 
 	cmp cx, 0 			;if there are no characters on the right side dont shitft them
@@ -158,10 +169,76 @@ shiftBufferRight:
 	ret
 
 
+shiftBufferLeft:
+
+	push edi
+	push esi
+	;cmp byte [esi], 0		
+	;je .continue
+
+	.shiftLoopLeft:
+	inc esi
+	cmp byte [esi], 0
+	je .continue
+	
+	mov dl, byte [esi]		;take character and move it to the left
+	dec esi
+	mov byte [esi], dl
+	inc esi
+
+	jmp .shiftLoopLeft
+
+
+	.continue:
+
+	dec esi
+	mov byte [esi], 0	;move 0 to end of buffer
+
+	call clear_screen
+
+	pop esi
+	pop edi
+
+	ret
+
+	;dec cx				;sets cx and esi to minus one so that the first loop doesnt affect them (in case that there are no characters on the right) 
+	;dec esi
+	;.checkBufferLoop:
+	;inc cx				;counts the number of characters in the buffer on the right side of the cursor
+	;inc esi
+	;mov dl, byte [esi]		;check for the end of the buffer
+	;cmp dl, 0
+	;jne .checkBufferLoop
+
+	;cmp cx, 0 			;if there are no characters on the right side dont shitft them
+	;je .continue
+	;dec esi
+
+	;.shiftLoop:
+	;mov dl, byte [esi]	
+	;inc esi
+	;mov byte [esi], dl		;store character in dl and move it one space to the right
+	;sub esi, 2			;go back two spaces to get the next character
+	;cmp cx, 2			;if there are two characters on the right side of the cursor jmp to lastshift
+	;je .lastShift
+
+	;dec cx
+	;jmp .shiftLoop			;repeat
+
+	;.lastShift:
+	;mov dl, byte [esi]
+	;inc esi
+	;mov byte [esi], dl
+	;dec esi				;only go one space back where the new character can be printed
+
+	;.continue:
+
+	;ret
+
 shutdownTextEditor:
 	call shutdown
 
 buffer: times BUFFER_LENGTH db 0
-db -1
+db 0
 
 END_PADDING
---
+++ paper/Introduction.asm
@@ -0,0 +1,2 @@
+Operating systems are essential for every working computer or mobile device. But what are operating systems exactly? In our group of three people we tried to answer that question as
+
