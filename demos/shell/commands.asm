d1c4b33b72b5f1e66e165808d8ba1c220141ec11
MorayYesilgueller <Moray___01@outlook.com>
2021-11-26 09:30:38 +0100

    Debugging

--- demos/shell/commands.asm
+++ demos/shell/commands.asm
@@ -2,3 +2,32 @@ buffer_shutdown:
 db "shutdown"
 times 247 db " "
 db 0
+buffer_hangman:
+db "hangman"
+times 248 db " "
+db 0
+buffer_time:
+db "time"
+times 250 db " "
+db 0
+buffer_paint:
+db "paint"
+times 250 db " "
+db 0
+buffer_text:
+db "text"
+times 251 db " "
+db 0
+buffer_cpuid:
+db "cpuid"
+times 250 db " "
+db 0
+
+
+
+
+
+
+
+
+
