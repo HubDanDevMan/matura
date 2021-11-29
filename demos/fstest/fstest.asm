02a5679214bf638881e3bff091256c513a855cd5
Daniel Huber <hubdan@protonmail.ch>
2021-11-29 13:00:04 +0100

    fixed file inclusion problem

--- demos/fstest/fstest.asm
+++ demos/fstest/fstest.asm
@@ -1,6 +1,42 @@
-%include "../src/filesystem.asm"
+jmp start
 
+; ReadSectors <CYL> <HEAD> <SECT> <NrOfSectors> <dest>
+%macro WriteSector 5
+        pusha
+        mov ax, 0
+        mov es, ax
+        mov ebx, %5             ; Dest
+        mov ah, 0x03            ; Set interrupt number
+        mov al, %4              ; Number of sectors
+        mov cx, %1              ; Cylinder
+        ror cx, 8
+        shl cl, 6
+        or cl, %3               ; Sector number
+        mov dh, %2              ; Head Number
+        mov dl, 0x80            ; Drive number, constant as of now
+        int 0x13
+	popa
+%endmacro
+ 
+; ReadSectors <CYL> <HEAD> <SECT> <NrOfSectors> <source>
+%macro ReadSector 5
+	pusha
+	mov ax, 0
+        mov es, ax
+        mov ebx, %5             ; Source
+        mov ah, 0x02            ; Set interrupt number
+        mov al, %4              ; Number of sectors
+        mov cx, %1              ; Cylinder
+        ror cx, 8
+        shl cl, 6
+        or cl, %3               ; Sector number
+        mov dh, %2              ; Head Number
+        mov dl, 0x80            ; Drive number, constant as of now
+        int 0x13
+	popa
+%endmacro
 
+start:
 mov al, "x"
 mov edi, 0x500
 mov cx, 512*2		; 2 sectors
@@ -14,10 +50,10 @@ call memset
 ;	 v
 
 WriteSector 0,0,1,2,0x500
-nop
-mov ah, 0x0e
-mov al, "D"
-int 0x10
+
+
+
+
 jmp $
 
 ; mov al, byte
